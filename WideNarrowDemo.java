2cf8f6fc0b23f9c70ba6f1b9423a862ad2c0eeea
MaheshDhumal7 <158827351+MaheshDhumal7@users.noreply.github.com>
2025-12-03 18:59:19 +0530

    Add files via upload

---
+++ WideNarrowDemo.java
@@ -0,0 +1,24 @@
+package widenarrowdemo;
+
+
+public class WideNarrowDemo {
+
+    
+    public static void main(String[] args) {
+        byte b=10;
+        short s=10;
+        int i=100;
+        float f=10f;
+        double d=10d;
+        long l=10;
+        char c=10;
+        boolean b1= true;
+        
+        i=(int)f; //Type casting
+        
+        System.out.println(i);
+        
+        
+    }
+    
+}
